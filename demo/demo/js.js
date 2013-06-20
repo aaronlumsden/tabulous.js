8eb45877e612d1d43354e7e696a3c03eb1bdc79a
Aaron Lumsden <aaronlumsden@me.com>
2013-06-20 19:19:54 +0100

    Initial commit

---
+++ demo/demo/js.js
@@ -0,0 +1,40 @@
+$(document).ready(function($) {
+    
+
+    $('#tabs').tabulous({
+    	effect: 'scale'
+    });
+
+     $('#tabs2').tabulous({
+    	effect: 'slideLeft'
+    });
+
+     $('#tabs3').tabulous({
+    	effect: 'scaleUp'
+    });
+
+    $('#tabs4').tabulous({
+    	effect: 'flip'
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
